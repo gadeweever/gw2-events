c353afbdbe0554daf560b5f41ed77f9dae971c2c
Gamal DeWeever <gadeweever@uchicago.edu>
2014-01-06 12:11:57 -0600

    project addition

---
+++ guildwarsEvents/guildwarsEvents/Globals.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guildwarsEvents
+{
+    public static class Globals
+    {
+        #region Variable Declarations
+        private static int _worldID;
+        private static int _mapID;
+        private static int _eventID;
+        #endregion
+
+        #region Variable Definitions
+        public static int WorldID
+        {
+            get
+            {
+                return _worldID;
+            }
+            set
+            {
+                _worldID = value;
+            }
+        }
+
+        public static int MapID
+        {
+            get
+            {
+                return _mapID;
+            }
+            set
+            {
+                _mapID = value;
+            }
+        }
+
+        public static int EventID
+        {
+            get
+            {
+                return _eventID;
+            }
+            set
+            {
+                _eventID = value;
+            }
+        }
+        #endregion
+
+
+
+    }
+}
---
+++ guildwarsEvents/guildwarsEvents/servers.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace guildwarsEvents
+{
+    public partial class servers : PhoneApplicationPage
+    {
+        
+        public servers()
+        {
+            InitializeComponent();
+            GetServerData();
+            
+        }
+
+        private void GetServerData()
+        {
+            string serverRequest = "https://api.guildwars2.com/v1/world_names.json";
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(serverRequest);
+            request.BeginGetResponse(RequestCallback, request);
+
+        }
+
+        private void RequestCallback(IAsyncResult result)
+        {
+            HttpWebRequest request = result.AsyncState as HttpWebRequest;
+            if (request != null)
+            {
+                try
+                {
+                    WebResponse response = request.EndGetResponse(result);
+                    serverList.ItemsSource = null;
+                    
+                }
+                catch (WebException e)
+                {
+                    Globals.WorldID = -1;
+                    return;
+                }
+            }
+
+        }
+
+        private void SetServer(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
